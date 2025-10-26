e48ff89369e64687b3859974b76f9ec526f73d69
Edmh <edavidmanotas@gmail.com>
2025-10-25 22:38:53 -0500

    Eliminar archivos y actualización gitignore

--- src/data_utils.py
+++ src/data_utils.py
@@ -5,7 +5,9 @@ import glob
 def cargar_excels():
     """
     Carga todos los archivos .xlsx desde data/raw y los une en un solo DataFrame.
-    Retorna: pandas.DataFrame
+
+    Returns: 
+        pandas.DataFrame
     """
     # Ruta absoluta a la carpeta data/raw
     resolved_path = Path(__file__).resolve().parent.parent
@@ -22,3 +24,19 @@ def cargar_excels():
     df_total = pd.concat(lista_df, ignore_index=True)
 
     return df_total
+
+def guardar_dataframe(df: pd.DataFrame, nombre_archivo: str):
+    """
+    Guarda un DataFrame en la carpeta data/processed como un archivo .csv.
+
+    Args:
+        df (pandas.DataFrame): El DataFrame a guardar.
+        nombre_archivo (str): El nombre del archivo (sin extensión).
+    """
+    # Ruta absoluta a la carpeta data/processed
+    resolved_path = Path(__file__).resolve().parent.parent
+    processed_path = resolved_path / 'data' / 'processed'
+    processed_path.mkdir(parents=True, exist_ok=True) 
+
+    ruta_guardado = processed_path / f"{nombre_archivo}.csv"
+    df.to_csv(ruta_guardado, index=False)
