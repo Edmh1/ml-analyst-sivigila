de47ceb5c1a4166bb0b2a9cc20814502521feeab
Edmh <edavidmanotas@gmail.com>
2025-10-29 02:18:37 -0500

    fix: vista del reporte

--- src/visualization_utils.py
+++ src/visualization_utils.py
@@ -1,9 +1,15 @@
 # URL del proyecto
 URL = "https://edmh1.github.io/ml-analyst-sivigila/"
-from IPython.display import HTML
 
-def show_report_link(url):
-    """Muestra un enlace HTML al reporte generado."""
-    url = URL + url
-    HTML(f'<a href="{url}" target="_blank">{url}</a>')
-    print(f"Reporte generado: {url}")
+def url_report(report_name):
+    """
+    Genera la URL completa para un reporte dado su nombre de archivo.
+
+    Args:
+        report_name (str): Nombre del archivo del reporte (por ejemplo, 'reports/reporte.html').
+
+    Returns:
+        str: URL completa del reporte.
+    
+    """
+    return f"{URL}{report_name}"
