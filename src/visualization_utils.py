51b14259a245d38131e09ae76dc5735d854705b3
Edmh <edavidmanotas@gmail.com>
2025-10-29 02:33:52 -0500

    refactor:  mejora modular de visualización

--- src/visualization_utils.py
+++ src/visualization_utils.py
@@ -1,3 +1,5 @@
+from IPython.display import display, Markdown
+
 # URL del proyecto
 URL = "https://edmh1.github.io/ml-analyst-sivigila/"
 
@@ -13,3 +15,16 @@ def url_report(report_name):
     
     """
     return f"{URL}{report_name}"
+
+def show_report_link(report_name, display_name=None):
+    """
+    Muestra un enlace al reporte en formato Markdown.
+
+    Args:
+        report_name (str): Nombre del archivo del reporte (por ejemplo, 'reports/reporte.html').
+        display_name (str, optional): Nombre a mostrar para el enlace. Si no se proporciona, se usa el nombre del archivo.
+    """
+    if display_name is None:
+        display_name = report_name
+    
+    display(Markdown(f"[{display_name}]({url_report(report_name)})"))
